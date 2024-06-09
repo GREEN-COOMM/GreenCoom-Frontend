e32008021911175b72064d49d517f4ea271b6f68
Luis Castillo <luisangelcastilloplasencia@gmail.com>
2024-06-09 11:13:16 -0500

    GREEN-05.1 Acualizacion

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EventService {
 
-  private apiUrl = 'http://localhost:8080/api/v1/events'; // Ajusta la URL de tu API
+  private apiUrl = 'http://localhost:8080/api/v1/event/create'; // Ajusta la URL de tu API
 
   constructor(private http: HttpClient) { }
 
